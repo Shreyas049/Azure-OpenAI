43d8dd78d4bc5ee0c6321096053e86d5d2a5b114
Shreyas049 <deshmukh.shreyas049@gmail.com>
2025-04-10 00:05:27 +0530

    genai_standard removed unnecessary configs

--- genai_standard/app/model/config.py
+++ genai_standard/app/model/config.py
@@ -1,9 +1,6 @@
 import os
 
 
-api_auth_name = "azure_openai_fastapi_auth_key"
-API_AUTH_KEY = "123456789"
-
 # azure openai resource configs
 endpt = os.getenv("azure_openai_endpt")
 key = os.getenv("azure_openai_key")
